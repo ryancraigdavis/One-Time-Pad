160a5c0359fe180d23be2183671b2194f98fc60b
Ryan Davis <ryancraigdavis@gmail.com>
2020-08-01 14:37:26 -0700

    finished encruption client and server

--- enc_client.c
+++ enc_client.c
@@ -6,24 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
-/**
-* Client code
-* 1. Create a socket and connect to the server specified in the command arugments.
-* 2. Prompt the user for input and send that input as a message to the server.
-* 3. Print the message received from the server and exit the program.
-*/
-
-// Error function used for reporting issues
-void error(const char *msg) { 
-  perror(msg); 
-  exit(0); 
-} 
+#include <stdbool.h>
 
 // Set up the address struct
-void setupAddressStruct(struct sockaddr_in* address, 
-                        int portNumber, 
-                        char* hostname){
+void setupAddressStruct(struct sockaddr_in* address, int portNumber, char* hostname){
  
   // Clear out the address struct
   memset((char*) address, '\0', sizeof(*address)); 
@@ -37,37 +23,14 @@ void setupAddressStruct(struct sockaddr_in* address,
   struct hostent* hostInfo = gethostbyname(hostname); 
   if (hostInfo == NULL) { 
     fprintf(stderr, "CLIENT: ERROR, no such host\n"); 
-    exit(0); 
+    exit(1); 
   }
+
   // Copy the first IP address from the DNS entry to sin_addr.s_addr
-  memcpy((char*) &address->sin_addr.s_addr, 
-        hostInfo->h_addr_list[0],
-        hostInfo->h_length);
+  memcpy((char*) &address->sin_addr.s_addr, hostInfo->h_addr_list[0], hostInfo->h_length);
 }
 
 int main(int argc, char *argv[]) {
-  int socketFD, portNumber, charsWritten, charsRead;
-  struct sockaddr_in serverAddress;
-
-  // Check usage & args
-  if (argc < 3) { 
-    fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
-    exit(0); 
-  } 
-
-  // Create a socket
-  socketFD = socket(AF_INET, SOCK_STREAM, 0); 
-  if (socketFD < 0){
-    error("CLIENT: ERROR opening socket");
-  }
-
-   // Set up the server address struct
-  setupAddressStruct(&serverAddress, atoi(argv[4]), argv[3]);
-
-  // Connect to server
-  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
-    error("CLIENT: ERROR connecting");
-  }
 
   // Variables for reading in the text from both the input file and key
   char * file_buffer = NULL;
@@ -76,16 +39,18 @@ int main(int argc, char *argv[]) {
   long file_length;
   long key_length;
   long buffer_length;
+
+  // File variables for the text file and key
   FILE *file_read;
   FILE *key_read;
 
   // Open the input file
-    file_read = fopen(argv[1], "r+");
+  file_read = fopen(argv[1], "r+");
 
-    // Used modfied code from this answer on reading a file into a string
-    // https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c
-    if (file_read) {
-    fseek (file_read, -1, SEEK_END);
+  // Used modfied code from this answer on reading a file into a string
+  // https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c
+  if (file_read) {
+    fseek (file_read, 0, SEEK_END);
     file_length = ftell(file_read);
     fseek (file_read, 0, SEEK_SET);
     file_buffer = malloc(file_length+1);
@@ -98,10 +63,33 @@ int main(int argc, char *argv[]) {
     fclose (file_read);
   }
 
+  // Remove the new line char at the end of the string and dec the file length by 1
+  if (strstr(file_buffer, "\n") != NULL) {
+    char* new_line = strstr(file_buffer, "\n");
+    *new_line = '\0';
+    file_length = file_length - 1;
+  }
+
+  // For loop goes through all of the input characters and verifies they aren't bad
+  for (int i = 0; i < file_length; ++i) {
+    int check_int = file_buffer[i] - 65;
+
+    // If the check int is below 0 and not -33 (space) then it is bad
+    if (check_int < 0 && check_int != -33) {
+      fprintf(stderr, "enc_client error: input contains bad characters\n");
+      exit(1);
+
+    // Or if the int is above 25
+    } else if (check_int > 25) {
+      fprintf(stderr, "enc_client error: input contains bad characters\n");
+      exit(1);
+    }
+  }
+
   // Same code as above, open the key file and read it in
   key_read = fopen(argv[2], "r+");
   if (key_read) {
-    fseek (key_read, -1, SEEK_END);
+    fseek (key_read, 0, SEEK_END);
     key_length = ftell(key_read);
     fseek (key_read, 0, SEEK_SET);
     key_buffer = malloc(key_length+1);
@@ -114,40 +102,110 @@ int main(int argc, char *argv[]) {
     fclose (key_read);
   }
 
-  // Check to see if the key is at least the length
+    // Remove the new line char at the end of the string and dec the key length by 1
+  if (strstr(key_buffer, "\n") != NULL) {
+    char* new_line_k = strstr(key_buffer, "\n");
+    *new_line_k = '\0';
+    key_length = key_length - 1;
+  }
+
+  // Check to see if the key is at least the length of the file
   if (key_length < file_length){
     fprintf(stderr, "Error: key ‘%s’ is too short\n",argv[2]);
     exit(1);
   }
 
   // Set buffer length and the buffer to a size of 2x the input file
-  buffer_length = file_length * 2;
-  buffer = malloc((file_length * 2) + 1);
+  // Buffer will start with a string ENC to differentiate it from the Dec client
+  // Buffer will end with @@ that will indicate the end of the string
+  buffer_length = (file_length * 2) + 5;
+  buffer = malloc((file_length * 2) + 6);
 
-  // Copy the input file into the buffer char var
+  // Copy in ENC first
+  strcpy(buffer, "ENC");
+
+  // Concat the input file into the buffer char var
   // Then concat it with key_buffer to the length of the input file
-  strcpy(buffer, file_buffer);
+  strcat(buffer, file_buffer);
   strncat(buffer, key_buffer, file_length);
 
-  // Send message to server
-  // Write to the server
-  charsWritten = send(socketFD, buffer, strlen(buffer), 0); 
-  if (charsWritten < 0){
-    error("CLIENT: ERROR writing to socket");
-  }
-  if (charsWritten < strlen(buffer)){
-    printf("CLIENT: WARNING: Not all data written to socket!\n");
+  // Finally add on the terminating chars
+  strcat(buffer, "@@");
+
+  // Create ints for reading/writing into the socket
+  // Create the socket address struct
+  int socketFD, portNumber, charsWritten, charsRead;
+  struct sockaddr_in serverAddress;
+
+  // Check usage & args - that there are enough arguments
+  if (argc < 2) { 
+    fprintf(stderr, "USAGE: %s hostname port\n", argv[0]); 
+    exit(1); 
+  } 
+
+  // Create a socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+  if (socketFD < 0){
+    fprintf(stderr, "CLIENT: ERROR opening socket");
+    exit(1);
   }
 
-  // Get return message from server
-  // Clear out the buffer again for reuse
-  memset(buffer, '\0', strlen(buffer));
-  // Read data from the socket, leaving \0 at end
-  charsRead = recv(socketFD, buffer, strlen(buffer) - 1, 0); 
-  if (charsRead < 0){
-    error("CLIENT: ERROR reading from socket");
+   // Set up the server address struct
+  setupAddressStruct(&serverAddress, atoi(argv[3]), "localhost");
+
+  // Connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+    fprintf(stderr, "CLIENT: ERROR connecting");
+    exit(1);
   }
-  printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
+
+  // Write to the server checking to make sure all the chars were written
+  charsWritten = send(socketFD, buffer, strlen(buffer), 0);
+  if (charsWritten < 0){
+    fprintf(stderr, "CLIENT: ERROR writing to socket");
+  }
+  if (charsWritten < strlen(buffer)){
+    fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+  }
+
+  // Set buffer to null terminated chars, creates a socket buffer to read in chunks
+  memset(buffer, '\0', buffer_length);
+  char socket_buffer[1000];
+
+  // Read the server's response from the socket using a loop until
+  // The terminating chars are found
+  while (strstr(buffer, "@@") == NULL){
+
+    // First check to see if the connection error message was sent
+    // This is an indication that there was a wrong client/server connection
+    // Print to stderr, and then exit(2)
+    if (strstr(buffer, "~~") != NULL) {
+      fprintf(stderr, "Error: could not contact enc_server on port %s\n", argv[3]);
+      exit(2);
+    }
+
+    // First clear the socket buffer
+    memset(socket_buffer, '\0', 1000);
+
+    // Read in the chunk from the socket
+    charsRead = recv(socketFD, socket_buffer, sizeof(socket_buffer) - 1, 0); 
+    if (charsRead < 0){
+      fprintf(stderr, "ERROR reading from socket\n");
+    }
+
+    // Add the chunk to the main buffer message
+    strcat(buffer, socket_buffer);
+  }
+
+  // Remove the terminating characters
+  char* terminalLoc = strstr(buffer, "@@");
+  *terminalLoc = '\0';
+
+  // Add a newline char at the end
+  strcat(buffer, "\n");
+
+  // Print to stdout
+  fprintf(stdout,"%s", buffer);
 
   // Close the socket
   close(socketFD); 
--- enc_server.c
+++ enc_server.c
@@ -5,34 +5,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-// Error function used for reporting issues
-void error(const char *msg) {
-  perror(msg);
-  exit(1);
-} 
+#include <stdbool.h>
 
 // Set up the address struct for the server socket
-void setupAddressStruct(struct sockaddr_in* address, 
-                        int portNumber){
+void setupAddressStruct(struct sockaddr_in* address, int portNumber){
  
   // Clear out the address struct
   memset((char*) address, '\0', sizeof(*address)); 
 
   // The address should be network capable
   address->sin_family = AF_INET;
+
   // Store the port number
   address->sin_port = htons(portNumber);
+
   // Allow a client at any address to connect to this server
   address->sin_addr.s_addr = INADDR_ANY;
 }
 
 int main(int argc, char *argv[]){
 
-  int connectionSocket, charsRead;
-  char buffer[256];
+  // Various variables for connecting, buffers, and the socket
+  // Connect bool is for checking that the right client is connecting
+  int connectionSocket, charsRead, charsWritten;
+  char buffer[150000];
+  char pipe_buffer[1000];
   struct sockaddr_in serverAddress, clientAddress;
   socklen_t sizeOfClientInfo = sizeof(clientAddress);
+  bool connect_bool = true;
 
   // Check usage & args
   if (argc < 2) { 
@@ -43,17 +43,17 @@ int main(int argc, char *argv[]){
   // Create the socket that will listen for connections
   int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (listenSocket < 0) {
-    error("ERROR opening socket");
+    fprintf(stderr, "ERROR opening socket");
+    exit(1);
   }
 
   // Set up the address struct for the server socket
   setupAddressStruct(&serverAddress, atoi(argv[1]));
 
   // Associate the socket to the port
-  if (bind(listenSocket, 
-          (struct sockaddr *)&serverAddress, 
-          sizeof(serverAddress)) < 0){
-    error("ERROR on binding");
+  if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
+    fprintf(stderr, "ERROR on binding");
+    exit(1);
   }
 
   // Start listening for connetions. Allow up to 5 connections to queue up
@@ -61,33 +61,174 @@ int main(int argc, char *argv[]){
   
   // Accept a connection, blocking if one is not available until one connects
   while(1){
+
     // Accept the connection request which creates a connection socket
-    connectionSocket = accept(listenSocket, 
-                (struct sockaddr *)&clientAddress, 
-                &sizeOfClientInfo); 
+    connectionSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); 
     if (connectionSocket < 0){
-      error("ERROR on accept");
+      fprintf(stderr, "ERROR on accept");
     }
 
-    printf("SERVER: Connected to client running at host %d port %d\n", 
-                          ntohs(clientAddress.sin_addr.s_addr),
-                          ntohs(clientAddress.sin_port));
+    // Set the buffer to null terminated chars, creates a socket buffer to
+    // read in chunks, and sets beginning_read to true
+    memset(buffer, '\0', 150000);
+    char socket_buffer[1000];
+    bool beginning_read = true;
 
-    // Get the message from the client and display it
-    memset(buffer, '\0', 256);
-    // Read the client's message from the socket
-    charsRead = recv(connectionSocket, buffer, 255, 0); 
-    if (charsRead < 0){
-      error("ERROR reading from socket");
-    }
-    printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+    // Read the client's message from the socket using a loop until
+    // the terminating chars are found
+    while (strstr(buffer, "@@") == NULL){
 
-    // Send a Success message back to the client
-    charsRead = send(connectionSocket, 
-                    "I am the server, and I got your message", 39, 0); 
-    if (charsRead < 0){
-      error("ERROR writing to socket");
+      // First clear the socket buffer
+      memset(socket_buffer, '\0', 1000);
+
+      // Read in the chunk from the socket
+      charsRead = recv(connectionSocket, socket_buffer, sizeof(socket_buffer) - 1, 0); 
+      if (charsRead < 0){
+        fprintf(stderr, "ERROR reading from socket\n");
+      }
+
+      // Checks to make sure that the connection is coming from the ENC client
+      // First checks to see if beginning read is true
+      if (beginning_read == true) {
+        char check_source[4];
+        strncpy(check_source, socket_buffer, 3);
+        int cmp_enc = strcmp(check_source, "ENC");
+        if (cmp_enc != 0) {
+
+          // If the first few chars aren't ENC, send an error message back to the
+          // client and close the connection by changing connect_bool
+          charsRead = send(connectionSocket, "~~ERROR: Source is Wrong", 22, 0); 
+          if (charsRead < 0){
+            fprintf(stderr, "ERROR reading from socket\n");
+          }
+
+          // Set connect bool to false, this skips down to the end where the connection is closed
+          connect_bool = false;
+          break;
+
+        // Otherwise, this is a valid connection, and change beginning_read to false
+        // So the loop can continue reading
+        } else {
+          beginning_read = false;
+        }
+      }
+
+      // Add the chunk to the main buffer message
+      strcat(buffer, socket_buffer);
     }
+
+    // Checks to make sure the connection is valid before proceeding
+    if (connect_bool == true) {
+
+      // Remove the terminal indicator of "@@"
+      char* terminal_buff = strstr(buffer, "@@");
+      *terminal_buff = '\0';
+
+      // Remove the first 3 chars of ENC from the string
+      char input_string[strlen(buffer)-2];
+      memset(input_string, '\0', strlen(buffer)-2);
+      strcpy(input_string, &buffer[3]);
+
+      // The received buffer string includes both the plain text and the key in one string
+      // Since each piece is the same length, the length of each piece is half of the whole
+      long input_length = strlen(input_string)/2;
+
+      // Create and null terminate a string for the plain text, key, and resulting cypher
+      char plain_text[input_length+1];
+      memset(plain_text, '\0', input_length+1);
+      char key[input_length+1];
+      memset(key, '\0', input_length+1);
+      char cypher_text[input_length+3];
+      memset(cypher_text, '\0', input_length+3);
+
+      // Copy the first half of the buffer to plain text and second half to the key
+      strncpy(plain_text, input_string, input_length);
+      strcpy(key, &input_string[input_length]);
+
+      // Creates the ints used to open the pipe - array is for input/output
+      int pipeProc[2];
+      int pipe_int;
+
+      // Create the pipe with error check
+      if (pipe(pipeProc) == -1) {
+        fprintf(stderr,"Call to pipe() failed\n"); 
+      }
+
+      // Fork a child process
+      pid_t childPid = fork();
+      if(childPid == 0){
+
+        // Go through each character of the text file and convert it to
+        // an int 0-26, do the same with the key
+        for (int i = 0; i < input_length; ++i) {
+          int text_int = plain_text[i] - 65;
+          if (text_int == -33) {
+            text_int = 26;
+          }
+          int key_int = key[i] - 65;
+          if (key_int == -33) {
+            key_int = 26;
+          }
+
+        // Add them together then put the modulus result in cypher_text
+          cypher_text[i] = ((text_int + key_int) % 26)+65;
+        }
+
+        // Add "@@" onto the end of the full buffer
+        strcat(cypher_text, "@@");
+
+        // Close the input pipe
+        close(pipeProc[0]);
+
+        // Write the entire string to the pipe
+        write(pipeProc[1], cypher_text, input_length+2); 
+
+        // Terminate the child
+        exit(0);
+        break;
+
+      } else {
+
+        // Parent process will read from the pipe, so close the output file desriptor.
+        close(pipeProc[1]);
+
+        // As long as we haven't found the terminal indicators @@
+        while (strstr(cypher_text, "@@") == NULL){
+
+          // Reset the pipe buffer
+          memset(pipe_buffer, '\0', 1000);
+
+          // Read the next chunk of data from the pipe - the '\0'
+          pipe_int = read(pipeProc[0], pipe_buffer, sizeof(pipe_buffer) - 1);
+
+          // Add the chunk we read to what we have read up to now
+          strcat(cypher_text, pipe_buffer); 
+          
+          // Check for errors
+          if (pipe_int == -1) { 
+            // -1 indicates an error
+            fprintf(stderr, "ERROR with reading from pipe\n"); 
+            break; 
+          } 
+          if (pipe_int == 0) {
+            // 0 indicates an error or end-of-file
+            fprintf(stderr, "ERROR end of file reached\n"); 
+            break; 
+          }
+        }
+      }
+
+      // Send the cyphertext back to the client's stdout with the @@ intact so that
+      // the client knows when the buffer has ended
+      charsWritten = send(connectionSocket, cypher_text, strlen(cypher_text), 0); 
+      if (charsWritten < 0){
+        fprintf(stderr, "ERROR writing to socket\n");
+      }
+      if (charsWritten < strlen(cypher_text)){
+        fprintf(stderr, "SERVER: WARNING: Not all data written to socket!\n");
+      }
+    }
+
     // Close the connection socket for this client
     close(connectionSocket); 
   }
